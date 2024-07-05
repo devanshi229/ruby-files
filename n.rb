117ed45b18248c8d0f2b24c4724cfac82bbbae1f
devanshi229 <devanshisavaliya5303@gmail.com>
2024-07-05 09:15:02 +0530

    Add files via upload

---
+++ n.rb
@@ -0,0 +1,11 @@
+age=25
+ if age>=18
+ 	puts "you are an adult"
+ 	if age>=21
+ 		puts"you are drive a car"
+ 	else
+ 		puts"you are not able to drive"
+ 	end
+ else
+ 	puts "you are not adult"
+ end
