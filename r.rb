1ce19e87f7d2dd3bf83f7eaa89ee647cf90a11f3
devanshi229 <devanshisavaliya5303@gmail.com>
2024-07-05 09:00:35 +0530

    Add files via upload

---
+++ r.rb
@@ -0,0 +1,18 @@
+a=10
+b=20
+c=15
+puts "value of A is:#{a} & C is:#{c}& B is:#{b}"
+
+puts "value of a+b=#{a+b+c}"
+#elsif
+	#puts "B is greater"
+#else
+    #puts "C is lesser"	
+#end
+if a>b&& a>c
+	puts "a is greater"
+elsif b>c
+	puts"b is greater"
+else
+	puts"c is greater"
+end
